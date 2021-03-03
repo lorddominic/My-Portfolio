5c86e542d02fd6f2e93397c2e4be048d3a0f9ef0
lorddominic <dominictxu1@gmail.com>
2021-03-03 15:58:19 -0700

    commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,8 +10,8 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Dominic Xu',
+  subtitle: 'JavaScript Developer',
   cta: '',
 };
 
